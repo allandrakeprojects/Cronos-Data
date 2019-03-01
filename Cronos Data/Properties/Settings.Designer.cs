7b9125728822be28df4e2d6acd990169707523be
SSIMAKATI\adulay <allandulay69@gmail.com>
2019-03-01 10:27:10 +0800

    update .60

--- Cronos Data/Properties/Settings.Designer.cs
+++ Cronos Data/Properties/Settings.Designer.cs
@@ -97,7 +97,7 @@ namespace FY_Cronos_Data.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("192.168.10.252")]
+        [global::System.Configuration.DefaultSettingValueAttribute("192.168.10.60")]
         public string ______server_ip {
             get {
                 return ((string)(this["______server_ip"]));
